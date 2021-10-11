5bf4b66731156c69abf9e3cc28b76d9922eddfa2
Sly <80170863+SlyVTT@users.noreply.github.com>
2021-10-11 07:51:28 +0200

    Delete synchrod directory

--- synchrod/ia1_ask.py
+++
@@ -1,8 +0,0 @@
-from polycal4 import get_infos
-from synchrod import *
-
-def tour(plateau, joueurs, evenements):
-    # Demander une action avec input()
-    return demander_action()
-
-play_game(tour)
